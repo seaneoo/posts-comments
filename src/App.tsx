4bbedd4a9a29b9856713ba777cf8064f59568375
Sean O'Connor <so@seano.dev>
2023-01-12 19:22:23 -0700

    Add react-query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <></>
     </QueryClientProvider>
   );
