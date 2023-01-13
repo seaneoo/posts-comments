4442c20ab5e23b025743bc0ed35cbd0232a8723a
Sean O'Connor <so@seano.dev>
2023-01-12 19:21:31 -0700

    [API] Create function `fetchResource`

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,17 @@
+import { formatStr } from "../utils";
 import { Post } from "./types";
+
+/**
+ * Fetches a resource at the provided URI. Optionally, you may provide a list of arguments to use in the URI.
+ *
+ * Ex: no argument: fetchResource("https://dummyjson.com/posts");
+ * with an argument: fetchResource("https://dummyjson.com/posts/{0}", id)
+ */
+const fetchResource = async <T>(uri: string, ...args: string[]) => {
+  return await fetch(formatStr(uri, ...args))
+    .then((res) => res.json())
+    .then((json) => json as T);
+};
 
 export const getPosts = async () => {
   return await fetch("https://dummyjson.com/posts")
