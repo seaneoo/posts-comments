f1446e1d7c7e7c6864d0be1ef1765cc8bfe44ffc
Sean O'Connor <so@seano.dev>
2023-01-12 20:15:54 -0700

    Add notice banner and add container for `Feed`

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,26 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Feed />
+      <div className="p-4 bg-red-500 border-red-700 text-center">
+        <span className="block text-white font-bold">
+          This project is a work in progress. Content is subject to change and
+          stuff may not work yet.
+        </span>
+        <span className="block text-white font-bold">
+          Check out the{" "}
+          <a
+            className="underline"
+            href="https://github.com/seaneoo/posts-comments/"
+          >
+            GitHub repo
+          </a>{" "}
+          for updates.
+        </span>
+      </div>
+
+      <div className="my-0 mx-auto p-8 w-1/2">
+        <Feed />
+      </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
