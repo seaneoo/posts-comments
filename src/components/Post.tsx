907e3ab0ed3368cc646a9e555efb7555f277b7b2
Sean O'Connor <so@seano.dev>
2023-01-16 10:15:14 -0700

    Add button to show comments

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCommentsOnPost } from "../api";
 import { Post as IPost } from "../api/types";
+import Comment from "./Comment";
 import User from "./User";
 
 type Props = {
@@ -6,6 +9,16 @@ type Props = {
 };
 
 export default function Post({ post }: Props) {
+  const { data, refetch } = useQuery(
+    ["comments", post.id],
+    () => getCommentsOnPost(post.id as unknown as string),
+    { refetchOnWindowFocus: false, enabled: false }
+  );
+
+  const showComments = () => {
+    refetch();
+  };
+
   return (
     <div className="block p-6 bg-white border border-gray-200 rounded-lg">
       <User userId={post.userId as unknown as string} />
@@ -17,6 +30,24 @@ export default function Post({ post }: Props) {
         <span className="py-1 px-2 bg-slate-100 border rounded">
           ❤️ {post.reactions}
         </span>
+      )}
+
+      {!data && (
+        <button
+          type="button"
+          className="block mt-4 py-1 px-2 bg-slate-100 border rounded"
+          onClick={showComments}
+        >
+          Show Comments
+        </button>
+      )}
+
+      {data && (
+        <div className="mt-4 flex flex-col gap-4">
+          {data.comments.map((comment, i) => (
+            <Comment key={i} comment={comment} />
+          ))}
+        </div>
       )}
     </div>
   );
